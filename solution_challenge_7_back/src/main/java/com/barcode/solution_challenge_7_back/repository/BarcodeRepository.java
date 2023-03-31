45d1c6d77246602cf9aef7ba97950bc11394eef0
jiyoungmerong <jiyoungmerong@gmail.com>
2023-04-01 02:48:52 +0900

    바코드 번호 수정

--- solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/repository/BarcodeRepository.java
+++ solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/repository/BarcodeRepository.java
@@ -4,9 +4,11 @@ import com.barcode.solution_challenge_7_back.domain.BarCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BarcodeRepository extends JpaRepository<BarCode, Integer> {
-    BarCode findByBarcodeNumber(String barcodeNumber);
+    Optional<BarCode> findByBarcodeNumber(String barcodeNumber);
 
 
 }

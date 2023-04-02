d77402296550d51a5db4b4469c4263e048fb2025
jiyoungmerong <jiyoungmerong@gmail.com>
2023-04-02 13:47:24 +0900

    yml 수정

--- solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/service/BarcodeService.java
+++ solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/service/BarcodeService.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -19,5 +21,6 @@ public class BarcodeService {
                 .orElseThrow(() -> new BarcodeNotFoundException(barcodeNumber));
         return new BarcodeDto(barCode.getGoods_name(), barCode.getHow(), barCode.getMethod());
     }
+
 
 }
